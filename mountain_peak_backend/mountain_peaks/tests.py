92ec78cd4f3c718943d3fd7136cd6017ed4d5abf
Simon Lyobard <simon.lyobard@gmail.com>
2022-11-06 21:20:24 +0100

    tests

--- mountain_peak_backend/mountain_peaks/tests.py
+++ mountain_peak_backend/mountain_peaks/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.geos import Point, Polygon
-from django.test import TestCase
+from django.test import Client, TestCase
 from mountain_peaks.models import MountainPeak
 
 # Create your tests here.
@@ -11,9 +11,18 @@ class MountainPeaksTestCase(TestCase):
         mont_blanc_coordinates = Point(x=6.865575, y=45.832119)
         cls.mont_blanc = MountainPeak.objects.create(name="Mont Blanc", altitude=4_696, coordinates=mont_blanc_coordinates)
 
-    def test_mountain_in_bbox(self):
-        poly = Polygon.from_bbox((6.745578979419631, 45.925101678711506, 7.092869, 45.72095))
-        self.assertTrue(self.mont_blanc in MountainPeak.objects.in_bbox(bbox=poly))
+    def setUp(self):
+        self.client = Client()
 
-        poly = Polygon.from_bbox((6.924254, 45.873148, 6.994573, 45.817374))
-        self.assertFalse(self.mont_blanc in MountainPeak.objects.in_bbox(bbox=poly))
+    def test_mountain_in_bbox(self):
+        response = self.client.get("/api/v1/mountains_peaks/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)  # type: ignore
+
+        response = self.client.get("/api/v1/mountains_peaks/?in_bbox=6.74,45.92,7.09,45.72")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)  # type: ignore
+
+        response = self.client.get("/api/v1/mountains_peaks/?in_bbox=6.92,45.87,6.99,45.81")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)  # type: ignore
