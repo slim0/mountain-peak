6e548a1de6370c0b5b4a71ebb1485d4f592396b3
Simon Lyobard <simon.lyobard@gmail.com>
2022-11-06 10:27:25 +0100

    Add tests

--- mountain_peak_backend/mountain_peaks/tests.py
+++ mountain_peak_backend/mountain_peaks/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.gis.geos import Point, Polygon
 from django.test import TestCase
+from mountain_peaks.models import Location, MountainPeak
 
 # Create your tests here.
+
+
+class MountainPeaksTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        mont_blanc_location = Location.objects.create(coordinates=Point(x=6.865575, y=45.832119))
+        cls.mont_blanc = MountainPeak.objects.create(name="Mont Blanc", altitude=4_696, location=mont_blanc_location)
+
+    def setUp(self):
+        ...
+
+    def test_mountain_in_bbox(self):
+        """Animals that can speak are correctly identified"""
+        poly = Polygon.from_bbox((6.745578979419631, 45.925101678711506, 7.092869, 45.72095))
+        self.assertTrue(self.mont_blanc in MountainPeak.objects.in_bbox(bbox=poly))
+
+        poly = Polygon.from_bbox((6.924254, 45.873148, 6.994573, 45.817374))
+        self.assertFalse(self.mont_blanc in MountainPeak.objects.in_bbox(bbox=poly))
