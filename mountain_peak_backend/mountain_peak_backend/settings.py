713c34633d1f281caf8b1a8a047052d61b16dac6
Simon Lyobard <simon.lyobard@gmail.com>
2022-11-06 09:51:06 +0100

    DEBUG & SECRET_KEY to env file

--- mountain_peak_backend/mountain_peak_backend/settings.py
+++ mountain_peak_backend/mountain_peak_backend/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_wtm)sopz((_ur$%k#-+pst6)@+nrw$ah+^k_jp+$m+@j9hvmk"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ["DEBUG"])
 
 ALLOWED_HOSTS = ["*"]
 
